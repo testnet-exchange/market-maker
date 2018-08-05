1522fd27a2469f171724e08ddc14b5081d02d43f
Dannil <danokhlopkov@gmail.com>
2018-08-05 19:47:58 +0300

    add config

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
-EXCHANGE_URL = "http://localhost:8080/
+EXCHANGE_URL = "http://localhost:8080/"
 BOT_USER_ID = -666
-MARKET = "TESTNET3RINKEBY"
+MARKET_NAME = "TESTNET3RINKEBY"
+MONEY_NAME = "TESTNET3"
+STOCK_NAME = "RINKEBY"
+TRADE_BALANCE_PRC = 0.1
+SPREAD_PRC = 0.1
 BTC_ETH_PRICE_URL = "https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=BTC"
