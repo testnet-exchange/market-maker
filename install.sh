ec811b9c10dbee2154fb62363c781b833f6080a5
Dannil <danokhlopkov@gmail.com>
2018-08-06 21:03:56 +0300

    change ln path

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 # install python API wrapper
 git clone https://github.com/testnet-exchange/python-viabtc-api
 pip3 install -r ./python-viabtc-api/requirements.txt 
-ln -s ../python-viabtc-api/ViaBTCAPI ViaBTCAPI
+ln -s python-viabtc-api/ViaBTCAPI ViaBTCAPI
 
 # install market-maker
 pip3 install -r requirements.txt
